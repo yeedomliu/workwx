4fd6b643c8dd10aad15f49dd9c950c2717b6931c
yeedomliu <wii_yeedomliu@tencent.com>
2018-04-24 15:15:02 +0800

    automated_submission

--- Card.php
+++ Card.php
@@ -10,23 +10,13 @@ namespace yeedomliu\workwx;
 class Card extends Base
 {
 
-    /**
-     * 获取打卡规则
-     *
-     * @link https://work.weixin.qq.com/api/doc#12423
-     *
-     * @param $datetime   需要获取规则的日期当天0点的Unix时间戳
-     * @param $useridlist 需要获取打卡规则的用户列表
-     *                    1.用户列表不超过100个，若用户超过100个，请分批获取。
-     *                    2.用户在不同日期的规则不一定相同，请按天获取。
-     *
-     * @return mixed
-     */
-    public function getOption($datetime, $useridlist) {
+    public function getData($opencheckindatatype, $starttime, $endtime, $useridlist) {
         return ((new Request())->setFields([
-                                               'datetime'   => $datetime,
-                                               'useridlist' => $useridlist,
-                                           ])->setUrl("checkin/getcheckinoption")->setPostMethod()->request());
+                                               'opencheckindatatype' => $opencheckindatatype,
+                                               'starttime'           => $starttime,
+                                               'endtime'             => $endtime,
+                                               'useridlist'          => $useridlist,
+                                           ])->setUrl("checkin/getcheckindata")->setPostMethod()->request());
     }
 
     /**
